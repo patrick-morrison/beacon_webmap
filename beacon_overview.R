53d6ae3502b2d9ffc38a6562da302c3f35a8f5bc
Patrick Morrison <patrick.morrison1498@gmail.com>
2021-06-15 15:13:43 +0800

    clean up after ImageOverlay

--- beacon_overview.R
+++ beacon_overview.R
@@ -2,26 +2,28 @@ library(leaflet)
 library(sf)
 library(geojsonsf)
 library(htmlwidgets)
-library(raster)
-library(leafem)
 
+#Burials
 bib <- geojson_sf("bib_shapes.geojson")
-
 popup = paste0( "<b>ID: ", bib$ID, "</b></br>",
                 "Description: " , bib$Description, "</br>",
                 "Sex: " , bib$Sex,"</br>",
-                "Stature: " , bib$Stature
-)
+                "Stature: " , bib$Stature)
 
-batavia <- geojson_sf("batavia_wrecksite.geojson")
+#National heritage list boundary
 nhl <- geojson_sf("nhl_boundary.geojson")
 
 map <- leaflet(bib, options = leafletOptions(preferCanvas = TRUE)) %>% 
-  addProviderTiles(providers$Esri.WorldImagery, options = providerTileOptions(minZoom = 8, maxZoom = 24), group="basemap") %>% 
+  addProviderTiles(providers$Esri.WorldImagery,
+                   options = providerTileOptions(minZoom = 8, maxZoom = 24),
+                   group="basemap") %>% 
   groupOptions("basemap", zoomLevels = 0:18) %>% 
   addPolygons(data=nhl, fill = FALSE, color = "coral") %>% 
-  addPolygons(popup=popup, popupOptions = popupOptions(maxWidth = 150)) %>% 
-  addCircles(data=batavia, color='green', label = "Batavia wreck site", labelOptions = labelOptions(noHide = T, direction = "bottom"))%>%
+  addPolygons(popup=popup,
+              popupOptions = popupOptions(maxWidth = 150)) %>% 
+  addCircles(113.7919,-28.49164, color='green',
+             label = "Batavia wreck",
+             labelOptions = labelOptions(noHide = T, direction = "bottom")) %>%
   fitBounds(113.788, -28.47561, 113.784, -28.47523) %>% 
   addScaleBar(position = 'bottomleft') %>% 
   addLegend("topright", 
@@ -29,6 +31,7 @@ map <- leaflet(bib, options = leafletOptions(preferCanvas = TRUE)) %>%
                     labels= c("Burial","NHL"),
                     opacity = 1) %>% 
   addMeasure() %>% 
+  #Add image overlays using custom javascript
   htmlwidgets::onRender("
       function(el, x) {
         console.log(this);
