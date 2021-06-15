b800f20eab2e44e89cbf3a941f01be5f91e799f8
Patrick Morrison <patrick.morrison1498@gmail.com>
2021-06-15 09:55:34 +0800

    Add files via upload

---
+++ beacon_overview.R
@@ -0,0 +1,34 @@
+library(leaflet)
+library(sf)
+library(geojsonsf)
+library(htmlwidgets)
+library(raster)
+library(leafem)
+
+bib <- geojson_sf("bib_shapes.geojson")
+
+popup = paste0( "<b>ID: ", bib$ID, "</b></br>",
+                "Description: " , bib$Description, "</br>",
+                "Sex: " , bib$Sex,"</br>",
+                "Stature: " , bib$Stature
+)
+
+batavia <- geojson_sf("batavia_wrecksite.geojson")
+nhl <- geojson_sf("nhl_boundary.geojson")
+beacon <- raster::stack('beacon.tif')# %>% raster::aggregate(1.2)
+
+map <- leaflet(bib, options = leafletOptions(preferCanvas = TRUE)) %>% 
+  addProviderTiles(providers$CartoDB, options = providerTileOptions(minZoom = 8, maxZoom = 24)) %>% 
+  #addRasterRGB(beacon, 1,2,3, group = "True colours", project = FALSE, maxBytes = 10 * 1024 * 1024,) %>%
+  addPolygons(data=nhl, fill = FALSE, color = "coral") %>% 
+  addPolygons(label = ~ID, popup=popup, popupOptions = popupOptions(maxWidth = 150)) %>% 
+  addMarkers(data=batavia, label = "Batavia wreck site", labelOptions(noHide = T, direction = "bottom"))%>%
+  fitBounds(113.788, -28.47561, 113.784, -28.47523) %>% 
+  addScaleBar(position = 'bottomleft') %>% 
+  addLegend("topright", 
+                    colors =c("blue", "coral"),
+                    labels= c("Burial","NHL Boundary"),
+                    opacity = 1)
+map
+saveWidget(map, file="beacon.html")
+
